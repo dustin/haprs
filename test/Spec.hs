490d35621fc4067bd14f7e5090e879f94b4e559f
dustin <dustin@spy.net>
2017-08-17 17:58:28 +0000

    Switch to test framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,29 +2,28 @@ import APRS.Types
 
 import Test.HUnit
 import Test.QuickCheck
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-testCallPass :: Test
-testCallPass = TestList (
-  map (\(s, want) -> TestCase (assertEqual s (callPass $ read s) want)) [
-      ("KG6HWF-9", 22955),
-      ("KG6HWF", 22955),
-      ("KE6AFE-13", 18595),
-      ("K6MGD", 12691)])
+testCallPass =
+  map (\(s, want) -> testCase s $ assertEqual s (callPass $ read s) want) [
+    ("KG6HWF-9", 22955),
+    ("KG6HWF", 22955),
+    ("KE6AFE-13", 18595),
+    ("K6MGD", 12691)]
 
-testAddressParsing :: Test
-testAddressParsing = TestList (
-  map (\(s, want) -> TestCase (assertEqual s (read s) want)) [
-      ("KG6HWF-11", address "KG6HWF" "11"),
-      ("KG6HWF", address "KG6HWF" ""),
-      ("KG6HWF-9", address "KG6HWF" "9")])
+testAddressParsing =
+  map (\(s, want) -> testCase s $ assertEqual s (read s) want) [
+    ("KG6HWF-11", address "KG6HWF" "11"),
+    ("KG6HWF", address "KG6HWF" ""),
+    ("KG6HWF-9", address "KG6HWF" "9")]
 
-tests :: Test
-tests = TestList [
-  TestLabel "callpass" testCallPass,
-  TestLabel "address" testAddressParsing
+tests = [
+  testGroup "callPass"  testCallPass,
+  testGroup "addrParse" testAddressParsing
   ]
 
-main :: IO Counts
-main = do runTestTT tests
+main = defaultMain tests
 
 

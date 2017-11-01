a82e247a87fa737734a714a452dcf148ec5428ef
Dustin Sallings <dustin@spy.net>
2017-10-31 21:36:48 -0700

    Added tests from FAP imports.

---
+++ test/FAPTests.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module FAPTests (tests) where
+
+import APRS.Types
+
+import Data.Aeson
+import Data.Maybe
+import Text.Read (readEither)
+import qualified Data.ByteString.Lazy as B
+
+import Test.HUnit (assertEqual, assertFailure)
+import Test.Framework (testGroup, Test)
+import Test.Framework.Providers.HUnit
+
+
+data Digipeater = Digipeater {
+  _wasdigied :: Int
+  , _call :: !String
+  } deriving (Show)
+
+instance FromJSON Digipeater where
+  parseJSON = withObject "Digipeater" $ \v -> Digipeater
+    <$> v .: "wasdigied"
+    <*> v .: "call"
+
+data FAPResult = FAPResult {
+  _symbolCode :: Maybe String
+  , _symbolTable :: Maybe String
+  , srcCallsign :: Maybe String
+  , dstCallsign :: Maybe String
+  , _latitude :: Maybe Double
+  , _longitude :: Maybe Double
+  , _posResolution :: Maybe Double
+  , _posAmbiguity :: Maybe Double
+  , body :: Maybe String
+  , _format :: Maybe String
+  , _messaging :: Maybe Int
+  , _origPacket :: Maybe String
+  , _typ :: Maybe String
+  , _header :: Maybe String
+  , _digipeaters :: Maybe [Digipeater]
+  } deriving (Show)
+
+instance FromJSON FAPResult where
+  parseJSON = withObject "FAPResult" $ \v -> FAPResult
+    <$> v .:? "symbolcode"
+    <*> v .:? "symboltable"
+    <*> v .:? "srccallsign"
+    <*> v .:? "dstcallsign"
+    <*> v .:? "latitude"
+    <*> v .:? "longitude"
+    <*> v .:? "posresolution"
+    <*> v .:? "posambiguity"
+    <*> v .:? "body"
+    <*> v .:? "format"
+    <*> v .:? "messaging"
+    <*> v .:? "origPacket"
+    <*> v .:? "type"
+    <*> v .:? "header"
+    <*> v .:? "digipeaters"
+
+data FAPTest = FAPTest {
+  src :: !String
+  , result :: Maybe FAPResult
+  , _failed :: Int
+  } deriving (Show)
+
+instance FromJSON FAPTest where
+    parseJSON = withObject "FAPTest" $ \v -> FAPTest
+        <$> v .: "src"
+        <*> v .:? "result"
+        <*> v .:? "failed" .!= 0
+
+fapTest :: FAPTest -> Test
+fapTest (FAPTest _ _ 1) = testGroup "" [ ] -- ignore failed ones
+fapTest f = case readEither (src f) :: Either String Frame of
+              Left e -> testCase "" (assertFailure (show e))
+              Right f' -> testGroup "" $ validate f'
+  where
+    validate :: Frame -> [Test]
+    validate (Frame s d _ b) =
+      [
+        testCase "" $ assertEqual "src" (fromJust.srcCallsign.fromJust $ result f) (show s),
+        testCase "" $ assertEqual "dst" (fromJust.dstCallsign.fromJust $ result f) (show d),
+        testCase "" $ assertEqual "body" (fromJust.FAPTests.body.fromJust $ result f) (show b)
+      ]
+
+tests :: IO [Test]
+tests = do
+  jstr <- B.readFile "test/faptests.json"
+  let tj = case eitherDecode jstr :: Either String [FAPTest] of
+             Right x -> x
+             Left x -> error ("decoding junk: " ++ show x)
+
+  return $ map fapTest tj
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,5 @@
 import APRSTests
+import FAPTests
 
 import Test.Framework.Runners.Options
 import Test.Framework.Options (TestOptions'(..))
@@ -7,6 +8,7 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main = do opts <- interpretArgsOrExit =<< getArgs
-          defaultMainWithOpts APRSTests.tests
+          faptests <- FAPTests.tests
+          defaultMainWithOpts (APRSTests.tests ++ faptests)
             opts { ropt_hide_successes = Just True,
                    ropt_test_options = Just $ mempty { topt_maximum_generated_tests = Just 500 }}

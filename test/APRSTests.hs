ba33f3b51dd92ddd8775161826631411a7153026
Dustin Sallings <dustin@spy.net>
2017-11-10 09:43:14 -0800

    Test message with no message ID

--- test/APRSTests.hs
+++ test/APRSTests.hs
@@ -224,6 +224,8 @@ testMegaParser =
           "!W26!/A=000188 14.3V 27C HDOP01.0 SATS09")),
   (":OH7LZB   :Testing, 1 2 3{1", Right (MessagePacket (raddr "OH7LZB")
                                          (Message' "Testing, 1 2 3") "1")),
+  (":OH7LZB   :Testing, 1 2 3", Right (MessagePacket (raddr "OH7LZB")
+                                       (Message' "Testing, 1 2 3") "")),
   (":OH7LZB   :ack1", Right (MessagePacket (raddr "OH7LZB") MessageACK "1")),
   (":OH7LZB   :rej1", Right (MessagePacket (raddr "OH7LZB") MessageNAK "1"))
 

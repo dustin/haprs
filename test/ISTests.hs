0d771ae0e0ff11895b77e04ab977084818da02fc
Dustin Sallings <dustin@spy.net>
2018-04-09 14:45:48 -0700

    Allow literal strings as Addresses in ISTests

--- test/ISTests.hs
+++ test/ISTests.hs
@@ -4,6 +4,7 @@
 
 module ISTests where
 
+import Data.String (IsString, fromString)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
@@ -14,27 +15,26 @@ import APRS.IS
 import APRS.Types (Address, Frame, Position(..), PosExtension(..))
 import APRS.Arbitrary ()
 
-raddr :: String -> Address
-raddr = read
+instance IsString Address where fromString = read
 
 testIDParser :: [TestTree]
 testIDParser =
   map (\(a, want) -> testCase (show a) $ assertEqual "" want (A.parseOnly parseIdentification a)) [
-  ("user KG6HWF-9 pass 11223 vers haprs 0.1 filter r/1", (Left "Failed reading: invalid callpass")),
-  ("user KG6HWF-9", (Left "call pass: not enough input")),
+  ("user KG6HWF-9 pass 11223 vers haprs 0.1 filter r/1", Left "Failed reading: invalid callpass"),
+  ("user KG6HWF-9", Left "call pass: not enough input"),
 
   ("user KG6HWF-9 pass -1 vers haprs 0.1 filter r/1",
-   (Right (id' (raddr "KG6HWF-9") (Just ("haprs", "0.1")) Nothing "filter r/1"))),
+   Right (id' "KG6HWF-9" (Just ("haprs", "0.1")) Nothing "filter r/1")),
   ("user KG6HWF-9 pass 22955 vers haprs 0.1 filter r/1",
-   (Right (id' (raddr "KG6HWF-9") (Just ("haprs", "0.1")) Nothing "filter r/1"))),
+   Right (id' "KG6HWF-9" (Just ("haprs", "0.1")) Nothing "filter r/1")),
   ("user KG6HWF-9 pass 22955 filter r/1",
-    (Right (id' (raddr "KG6HWF-9") Nothing Nothing "filter r/1"))),
+    Right (id' "KG6HWF-9" Nothing Nothing "filter r/1")),
   ("user KG6HWF-9 pass 22955",
-    (Right (id' (raddr "KG6HWF-9") Nothing Nothing ""))),
+    Right (id' "KG6HWF-9" Nothing Nothing "")),
   ("user KG6HWF-9 pass 22955 UDP 8425 filter r/1",
-   (Right (id' (raddr "KG6HWF-9") Nothing (Just 8425) "filter r/1"))),
+   Right (id' "KG6HWF-9" Nothing (Just 8425) "filter r/1")),
   ("user KG6HWF-9 pass 22955 vers haprs 0.1 UDP 1384 filter r/1",
-    (Right (id' (raddr "KG6HWF-9") (Just ("haprs", "0.1")) (Just 1384) "filter r/1")))
+    Right (id' "KG6HWF-9" (Just ("haprs", "0.1")) (Just 1384) "filter r/1"))
   ]
 
   where id' a b c d = ID a b c d undefined
@@ -55,7 +55,7 @@ testFilterParser =
   ("filter p/KG/K6",
    Right (Filter [PrefixFilter ["KG", "K6"]])),
   ("filter b/KG6HWF/K6MGD",
-   Right (Filter [BudlistFilter $ map raddr ["KG6HWF", "K6MGD"]])),
+   Right (Filter [BudlistFilter ["KG6HWF", "K6MGD"]])),
   ("filter o/BAYLANDS/SOMETHING",
    Right (Filter [ObjectFilter ["BAYLANDS", "SOMETHING"]])),
   ("filter os/BAYLANDS/SOMETHING",
@@ -63,7 +63,7 @@ testFilterParser =
   ("filter t/pow",
    Right (Filter [TypeFilter "pow" Nothing])),
   ("filter t/pow/KG6HWF/13",
-   Right (Filter [TypeFilter "pow" (Just (raddr "KG6HWF", 13))])),
+   Right (Filter [TypeFilter "pow" (Just ("KG6HWF", 13))])),
   ("filter s/->",
    Right (Filter [SymbolFilter "->" "" ""])),
   ("filter s//#",
@@ -71,20 +71,20 @@ testFilterParser =
   ("filter s//#/T",
    Right (Filter [SymbolFilter "" "#" "T"])),
   ("filter d/KG6HWF-1/KG6HWF-3",
-   Right (Filter [DigiFilter $ map raddr ["KG6HWF-1", "KG6HWF-3"]])),
+   Right (Filter [DigiFilter ["KG6HWF-1", "KG6HWF-3"]])),
   ("filter a/38/37/-122/-121",
    Right (Filter [AreaFilter 38 37 (-122) (-121)])),
   ("filter e/KG6HWF-3/K6MGD-5",
-   Right (Filter [EntryStationFilter $ map raddr ["KG6HWF-3", "K6MGD-5"]])),
+   Right (Filter [EntryStationFilter ["KG6HWF-3", "K6MGD-5"]])),
   ("filter g/KG6HWF/K6MGD/KI6IQI",
-   Right (Filter [GroupMessageFilter $ map raddr ["KG6HWF", "K6MGD", "KI6IQI"]])),
+   Right (Filter [GroupMessageFilter ["KG6HWF", "K6MGD", "KI6IQI"]])),
   ("filter u/KG6HWF-3",
-   Right (Filter [UnprotoFilter $ map raddr ["KG6HWF-3"]])),
+   Right (Filter [UnprotoFilter ["KG6HWF-3"]])),
   ("filter q/C", Right (Filter [QConsFilter "C" False])),
   ("filter q/rR", Right (Filter [QConsFilter "rR" False])),
   ("filter q//I", Right (Filter [QConsFilter "" True])),
   ("filter m/13", Right (Filter [MyRangeFilter 13])),
-  ("filter f/K6MGD/19.3", Right (Filter [FriendRangeFilter (raddr "K6MGD") 19.3])),
+  ("filter f/K6MGD/19.3", Right (Filter [FriendRangeFilter "K6MGD" 19.3])),
   ("filter m/200 -p/CW",
    Right (Filter [MyRangeFilter 200.0,NotFilter (PrefixFilter ["CW"])])),
   ("filter r/33/-97/200 t/n",
@@ -108,5 +108,5 @@ tests = [
 
   testProperty "filter round tripping" (propParseRoundTrip parseFilter),
   testProperty "filter item round tripping" (withMaxSuccess 1000 $ propParseRoundTrip parseFilterItem),
-  testProperty "no double negative filters" (withMaxSuccess 1000 $ propNoDoubleNegatives)
+  testProperty "no double negative filters" (withMaxSuccess 1000 propNoDoubleNegatives)
   ]
